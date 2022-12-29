c9b7184f7c1e559d807b2dfc1fd753405886557e
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-29 19:20:19 +0300

    Add project files.

---
+++ CodeGen/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisi k = new Kisi {Id=1, Ad="Ali", Soyad="Diker" };
+            var propList = typeof(Kisi).GetProperties();
+            List<string> proJlist = new List<string>();
+
+            foreach (var item in propList)
+            {
+                object val = item.GetValue(k);
+                if (item.PropertyType == typeof(string))
+                {
+                    proJlist.Add(String.Format("{0} \"{1}\"", item.Name, val));
+                }
+                else
+                {
+                    proJlist.Add(String.Format("{0}: {1}", item.Name, val));
+                }                             
+            }
+            string final = "{" + string.Join(", \r\n", proJlist) + "}";
+            Console.WriteLine(final);
+            Console.ReadLine();
+            
+        }
+    }
+    public class Kisi
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TamAdi
+        {
+            get { return Ad + " " + Soyad; }
+
+        }
+    }
+}
